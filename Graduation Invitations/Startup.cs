78cbcd3fd3285e4af878c4633f92daa01461109a
Evan McNeal <Evan McNeal@IRON-MAN>
2017-04-03 10:31:02 -0500

    Add project files.

---
+++ Graduation Invitations/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Graduation_Invitations.Startup))]
+namespace Graduation_Invitations
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
